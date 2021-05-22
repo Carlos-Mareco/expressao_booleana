d1f6db0e5d6b16bde1651c58199199e2cbb8acc7
Carlos H. B. Mareco <carlosmareco2001@gmail.com>
2021-05-22 02:31:48 -0300

    Código inteiramente comentado

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cmath>
-#include <fstream>
+#include <cmath> //Necessário para log2(n) e pow(2, n)
+#include <fstream> //Necessário para ler o arquivo de texto
 
 using namespace std;
 
@@ -9,9 +9,9 @@ string expressaoBooleana(string saida, int n_entradas);
 string convDecToBin(int valor, int tam);
 
 int main(){
-    string saida = lerSaida("input.txt");
-    int n_entradas = log2(saida.length());
-    string expressao = expressaoBooleana(saida, n_entradas);
+    string saida = lerSaida("input.txt"); //ler o arquivo de nome "input.txt" e retorna uma string
+    int n_entradas = log2(saida.length()); // n = log2(x), onde x é a quant. de saídas e n é a quant. de entradas
+    string expressao = expressaoBooleana(saida, n_entradas); //Guarda a expressão booleana
     
     cout << expressao << endl;
     return 0;
@@ -34,28 +34,33 @@ string lerSaida(string nomeArquivo){
     }
     return cadeiaSaida;
 }
+//Gerador de expressão booleana
 string expressaoBooleana(string saida, int n_entradas){
-    int linhas = pow(2, n_entradas);
+    int linhas = pow(2, n_entradas); //Gera 2^n linhas válidas, onde n é a quantidade de entradas
     string cadeia, expressao;
-    string variaveis = "ABCDEF"; //Variáveis disponíveis para cada entrada
-    bool sinalAnd = false;
+    string variaveis = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Variáveis disponíveis para entradas
+    bool sinalAnd = false; //Sinalizador para adicionar o sinal +
     
     for(int i = 0; i < linhas; i++){
         if(saida[i] == '1'){
-            if(sinalAnd)
+            if(sinalAnd) //Não adiciona o sinal + no primeiro termo
                 expressao += " + ";
-            sinalAnd = true;
-            cadeia = convDecToBin(i, n_entradas);
+            sinalAnd = true; //Adiciona o sinal antes do próximo termo
+            // O valor de i é um número que convertido em binário fornece os valores de cada entrada.
+            // Exemplo: Se a saída for '1' quando i = 3 e a quant. de entradas for 3, então as entradas são 0, 1 e 1
+            cadeia = convDecToBin(i, n_entradas); 
+            //Ler as entradas de cada variável de trás para frente, visto que os bits estão invertidos em 'cadeia'
+            //Exemplo: Quando i = 4, n_entradas = 3, então cadeia será 001 ao invés de 100
             for(int j = n_entradas - 1; j >= 0; j--){
-                expressao += variaveis[n_entradas - j -1];
-                if(cadeia[j] == '0')
+                expressao += variaveis[n_entradas - j -1]; //Imprime uma variável de cada posição de 'variáveis'
+                if(cadeia[j] == '0') // Se a entrada for 0, então ele nega a variável da entrada
                     expressao += "'";
             }
         }
     }
     return expressao;
 }
-//COnverte decimal para binário
+//Converte decimal para binário e retorna uma string INVERTIDA com uma cadeia de bits com tamanho 'tam'
 string convDecToBin(int valor, int tam){
     string bin;
     for(int i = 0; i < tam; i++){
